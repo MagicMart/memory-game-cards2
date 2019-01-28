0bd0312ca222bfc7705031b5cd240b661a6cc7a4
Martin Tudor <martcan27@gmail.com>
2019-01-28 19:54:09 +0000

    feature: cards can be matched

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,40 +10,86 @@ class Cards extends React.Component {
         this.state = {
             cards: [],
             holdCards: [],
-            heldCards: 0
+            cardClass: []
         };
-        this.handleClick = this.handleClick.bind(this);
+        this.openCard = this.openCard.bind(this);
     }
 
     componentDidMount() {
-        this.setState({ cards: shuffleArray(cardArray) });
+        this.setState({
+            cards: shuffleArray(cardArray),
+            cardClass: new Array(16).fill("card")
+        });
     }
 
-    handleClick(e) {
-        const target = e.currentTarget;
-        if (
-            target.className === "card open show" ||
-            target.className === "card open match"
-        ) {
+    matchCards() {
+        const { holdCards } = this.state;
+        const update = this.state.cardClass.map((el, j) => {
+            return holdCards[0].i === j || holdCards[1].i === j
+                ? "card open match"
+                : el;
+        });
+        this.setState({ cardClass: update, holdCards: [] });
+    }
+
+    closeCards() {
+        const { holdCards } = this.state;
+        const update = this.state.cardClass.map((el, j) => {
+            return holdCards[0].i === j || holdCards[1].i === j ? "card" : el;
+        });
+        this.setState({ cardClass: update, holdCards: [] });
+    }
+
+    checkCards() {
+        const { holdCards } = this.state;
+        if (holdCards[0].card === holdCards[1].card) {
+            this.matchCards();
+        } else {
+            window.setTimeout(() => this.closeCards(), 600);
+        }
+    }
+
+    componentDidUpdate(prevState) {
+        if (prevState.holdCards === this.state.holdCards) {
             return;
         }
-        target.className = "card open show";
+        if (this.state.holdCards.length > 1) {
+            this.checkCards();
+        } else {
+            return;
+        }
+    }
+
+    openCard(obj) {
+        const { card, i, cardClass } = obj;
+        if (cardClass === "card open show" || cardClass === "card open match") {
+            return;
+        }
+        const updateCardClass = this.state.cardClass.map((el, j) => {
+            return j === i ? "card open show" : el;
+        });
         this.setState(function(state) {
             return {
-                holdCards: [...state.holdCards, target],
-                heldCards: state.heldCards + 1
+                holdCards: [...state.holdCards, { card: card, i: i }],
+                // heldCards: state.heldCards + 1,
+                cardClass: updateCardClass
             };
         });
     }
+
     render() {
         const cards = this.state.cards.map(
             function(card, i) {
                 return (
                     <li
                         key={card + i}
-                        className="card"
-                        onClick={function(e) {
-                            this.handleClick(e);
+                        className={this.state.cardClass[i]}
+                        onClick={function() {
+                            this.openCard({
+                                card: card,
+                                i: i,
+                                cardClass: this.state.cardClass[i]
+                            });
                         }.bind(this)}
                     >
                         <i className={card} />
