a29df0d32fef15226801d2a7cb1e74479f5fe554
Martin Tudor <martcan27@gmail.com>
2019-01-29 16:03:57 +0000

    new component Seconds

---
+++ src/components/Seconds.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Seconds extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            seconds: 0
+        };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+        this.props.finishTime(this.state.seconds);
+    }
+
+    tick() {
+        this.setState(state => ({ seconds: state.seconds + 1 }));
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <span className="seconds">
+                    {String(this.state.seconds).padStart(3, 0)}
+                </span>
+                <small> Secs </small>
+            </React.Fragment>
+        );
+    }
+}
+
+Seconds.propTypes = {
+    finishTime: PropTypes.func.isRequired
+};
+
+export default Seconds;
