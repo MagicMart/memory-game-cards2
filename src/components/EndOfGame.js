63318b08d2120473d6f44adbe0ae7faf1d61ff75
Martin Tudor <martcan27@gmail.com>
2019-01-30 20:41:27 +0000

    new component: EndOfGame

---
+++ src/components/EndOfGame.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function EndOfGame() {
+    return (
+        <div className="end-of-game">
+            <div className="final-figures">
+                <h1>End of Game</h1>
+            </div>
+        </div>
+    );
+}
+
+export default EndOfGame;
