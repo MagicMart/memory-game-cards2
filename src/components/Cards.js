28aa7de0f3efc8f63a4ae4c72984ac2b8a94a5be
Martin Tudor <martcan27@gmail.com>
2019-02-04 23:10:52 +0000

    remove obsolete variable

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -44,7 +44,6 @@ class Cards extends React.Component {
             cardClass: []
         };
         this.openCard = this.openCard.bind(this);
-        this.checkingPhase = false;
     }
 
     componentDidMount() {
